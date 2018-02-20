f6ed4a2de08d94b838dd852ff466b1ae1d6be802
SarCasm <sarcasm008@gmail.com>
2018-02-20 19:49:45 +0200

    Fix seeds

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,4 +1,4 @@
-c = Customer.first_or_create(name:'Trail of Bits', email:"nick.esposito@trailofbits.com", password:'dev', password_confirmation:'dev')
+c = Customer.first_or_create(name:'Trail of Bits', email:"nick.esposito@trailofbits.com", encrypted_password:'dev')
 a = CustomerApplication.create(name:'Javelin', customer: c)
 
 puts c.api_key
